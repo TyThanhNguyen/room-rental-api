668f90455ee762a3e69a90950d92408bf404f3a1
ThanhTy <developnguyen@gmail.com>
2018-07-05 17:49:03 +0800

    update room model

--- server/models/room.js
+++ server/models/room.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 var RoomSchema = new mongoose.Schema({
-    description: {
-        type: String,
-        required: true,
-    },
     imagePath: {
         type: String,
         required: true
@@ -13,16 +9,47 @@ var RoomSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        default: ''
+    },
     price: {
         type: Number,
         required: true,
+        default: 0.00
     },
-    // moveInDate: {
-    //     type: Date
-    // },
-    // moveOutDate: {
-    //     type: Date
-    // }
+    moveInDate: {
+        type: Date
+    },
+    moveOutDate: {
+        type: Date
+    },
+    facilities: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: true
+    },
+    billIncluded: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: true
+    },
+    securityAndSafety: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: false
+    },
+    propertyRule: {
+        type: [mongoose.Schema.Types.ObjectId],
+        required: false
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 let Room = mongoose.model('Room', RoomSchema);
