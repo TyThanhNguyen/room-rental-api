d2aa9cb174e6f29c2d3239479fb9456afd22887f
ThanhTy <developnguyen@gmail.com>
2018-07-06 21:21:44 +0800

    add existVerify method

--- server/models/facility.js
+++ server/models/facility.js
@@ -7,5 +7,16 @@ let FacilitySchema = new mongoose.Schema({
     }
 });
 
+// model methods
+FacilitySchema.statics.existVerify = function(item) {
+    let facility = this;
+    return Facility.findOne({item}).then((facility) => {
+        if (facility === null) {
+            return Promise.resolve();
+        }
+        return Promise.reject('Exist');
+    });
+};
+
 let Facility = mongoose.model('Facility', FacilitySchema);
 module.exports = { Facility };
