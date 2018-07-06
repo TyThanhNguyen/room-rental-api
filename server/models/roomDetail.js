7e08f82613bbb6a5fe3e4f2c0182f5d7ce0fb41d
ThanhTy <developnguyen@gmail.com>
2018-07-06 17:42:31 +0800

    add existVerify method model in roomDetail model

--- server/models/roomDetail.js
+++ server/models/roomDetail.js
@@ -10,5 +10,16 @@ let RoomDetailSchema = new mongoose.Schema({
     }
 });
 
+// model methods
+RoomDetailSchema.statics.existVerify = function(name) {
+    let roomDetail = this;
+    return RoomDetail.findOne({name}).then((roomDetail) => {
+        if (roomDetail === null) {
+            return Promise.resolve();
+        }
+        return Promise.reject('Exist')
+    });
+}
+
 let RoomDetail = mongoose.model('RoomDetail', RoomDetailSchema);
 module.exports = { RoomDetail };
