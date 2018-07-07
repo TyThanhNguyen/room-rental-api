820a8903b410300dc212bd110bac939012f103bd
ThanhTy <developnguyen@gmail.com>
2018-07-07 12:38:38 +0800

    add existVerify method

--- server/models/billIncluded.js
+++ server/models/billIncluded.js
@@ -7,5 +7,16 @@ let BillIncludedSchema = new mongoose.Schema({
     }
 });
 
+// model methods
+BillIncludedSchema.statics.existVerify = function(item) {
+    let billIncluded = this;
+    return BillIncluded.findOne({item}).then((billIncluded) => {
+        if(!billIncluded) {
+            return Promise.resolve();
+        }
+        return Promise.reject('Exist');
+    });
+};
+
 let BillIncluded = mongoose.model('BillIncludedSchema', BillIncludedSchema);
 module.exports = { BillIncluded };
